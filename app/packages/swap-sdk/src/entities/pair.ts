6f524c2b5201984f96c3c1efea8f42fd4199216f
dyl <yunli.ding@mimos.io>
2023-06-13 15:05:29 +0800

    fix: modify text

--- app/packages/swap-sdk/src/entities/pair.ts
+++ app/packages/swap-sdk/src/entities/pair.ts
@@ -64,8 +64,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'Cake-LP',
-      'Pancake LPs'
+      'PE-LP',
+      'EquitySwap LPs'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
